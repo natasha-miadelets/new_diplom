01382e14f418edf5c4cde9eaa3a4828664ba98b9
NatashaMiadelets <nmiadelets@remedypointsolutions.com>
2021-10-01 16:42:52 +0300

    edit locator

--- locators/Login_Page_Locators.py
+++ locators/Login_Page_Locators.py
@@ -3,7 +3,7 @@ from selenium.webdriver.common.by import By
 
 class LoginPageLocators:
 
-    LOCATOR_HEAD_TEXT = (By.XPATH, "//a[text()='Django administrationn']")
+    LOCATOR_HEAD_TEXT = (By.XPATH, "//a[text()='Django administration']")
     LOCATOR_USERNAME_FIELD = (By.XPATH, "//input[@name='username']")
     LOCATOR_PASSWORD_FIELD = (By.XPATH, "//input[@name='password']")
     LOCATOR_LOGIN_BUTTON = (By.XPATH, "//input[@type='submit']")
