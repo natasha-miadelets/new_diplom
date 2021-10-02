cc4baf8466ffc220a8077238f648b4afcd54f125
NatashaMiadelets <nmiadelets@remedypointsolutions.com>
2021-10-02 11:15:14 +0300

    last edit

--- tests/test_api.py
+++ tests/test_api.py
@@ -9,12 +9,12 @@ PET_NEW_NAME = "MIKI_NEW"
 
 @allure.feature('API test')
 def test_add_update_pet():
-    with allure.step(f'add pet with random {PET_ID} in the store'):
+    with allure.step(f'add pet with random id {PET_ID} in the store'):
         api_page = Api()
         api_page.add_new_pet(PET_ID, PET_NAME)
     with allure.step('check that pet is added'):
         api_page.check_added_pet(PET_ID)
-    with allure.step('update pet name "MIKI" to "MIKI_NEW"'):
+    with allure.step(f'update pet name {PET_NAME} to {PET_NEW_NAME}'):
         api_page.update_pet_name(PET_ID, PET_NEW_NAME)
     with allure.step('check that pet name is updated'):
         api_page.check_updated_name(PET_ID, PET_NEW_NAME)
